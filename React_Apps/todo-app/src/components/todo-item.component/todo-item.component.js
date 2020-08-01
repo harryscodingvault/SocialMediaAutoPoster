1b81c803826117a61808c207edd532ee6196de8f
HaroldDRF <95robots@gmail.com>
2020-07-31 23:54:35 -0700

    2nd commit

--- React_Apps/todo-app/src/components/todo-item.component/todo-item.component.js
+++ React_Apps/todo-app/src/components/todo-item.component/todo-item.component.js
@@ -4,7 +4,7 @@ export default class Item extends Component {
     render() {
         return (
             <div>
-                
+                <h1>qwe</h1>
             </div>
         )
     }
