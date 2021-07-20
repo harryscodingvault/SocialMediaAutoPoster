9348fad6f35b3e708193c614bc19ec77737b5823
HaroldDRF <95robots@gmail.com>
2021-07-20 00:44:20 -0700

    Login and signup pages and views created

--- Django/DogBlogDjango/dog_blog/App_Login/urls.py
+++ Django/DogBlogDjango/dog_blog/App_Login/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+app_name = 'App_Login'
 
+urlpatterns = [
+    path('signup/', views.sign_up, name='signup'),
+    path('login/', views.login_page, name='login')
 ]
