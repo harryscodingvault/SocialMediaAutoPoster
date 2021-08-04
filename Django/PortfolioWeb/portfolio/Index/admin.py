10315d76542a460b5ca6051fbdd8116f7fcc9846
HaroldDRF <95robots@gmail.com>
2021-08-04 14:26:52 -0700

    var upgrade

---
+++ Django/PortfolioWeb/portfolio/Index/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import About, Intro, Social, Projects, Skills
+
+# Register your models here.
+admin.site.register(About)
+admin.site.register(Intro)
+admin.site.register(Social)
+admin.site.register(Projects)
+admin.site.register(Skills)
