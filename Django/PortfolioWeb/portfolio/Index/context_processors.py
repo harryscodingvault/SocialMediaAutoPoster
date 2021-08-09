b1f0d0c28afd596b24e2db36ab3257d177b70e97
Harold DR <95robots@gmail.com>
2021-08-09 15:39:06 -0700

    Delete Django directory

--- Django/PortfolioWeb/portfolio/Index/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import Social
-
-def footer_nav(request):
-    social_data = Social.objects.all()
-    context = {
-        'social': social_data,
-    }
-    return context
--- Django/SeleniumJobAuto/utils/utils.py
+++
@@ -1,6 +0,0 @@
-import random
-from time import sleep
-
-def rest():
-    print('sleep')
-    sleep(random.randint(0,3))
