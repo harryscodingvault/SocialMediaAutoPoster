3b0bf2e0e6cf60fba20b20dbf0ed6a86cd7de744
HaroldDRF <95robots@gmail.com>
2021-08-09 18:28:27 -0700

    Account and Category models improved

--- ecomDjango/ecomSite/ecomSite/settings.py
+++ ecomDjango/ecomSite/ecomSite/settings.py
@@ -127,6 +127,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [ 'ecomSite/static', ]
 
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
