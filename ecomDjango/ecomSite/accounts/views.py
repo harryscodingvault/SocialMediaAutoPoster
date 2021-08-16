e88749e5cce076b22aa373991ca429590d148932
HaroldDRF <95robots@gmail.com>
2021-08-15 23:56:59 -0700

    Registration form implemented in views

--- ecomDjango/ecomSite/accounts/views.py
+++ ecomDjango/ecomSite/accounts/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
-
+from .models import Account
 
 # Create your views here.
 def register(request):
-    form = RegistrationForm()
+    if request.metod == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+
+            user = Account.objects.create_user(username=username, email=email, password=password)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
     context = {
         'form':form,
     }
