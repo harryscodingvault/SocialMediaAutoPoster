9954e963571698c5c4bb9e73161fc206eb8f4ee5
HaroldDRF <95robots@gmail.com>
2021-08-15 23:45:59 -0700

    Native form view and html overritten

--- ecomDjango/ecomSite/accounts/forms.py
+++ ecomDjango/ecomSite/accounts/forms.py
@@ -12,3 +12,12 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['username', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter username'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
