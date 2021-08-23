5bb07e00e400547967e99299f5b483a903587104
HaroldDRF <95robots@gmail.com>
2021-08-23 16:22:32 -0700

    navbar style completed

--- ecomDjango/ecomSite/ecomSite/settings.py
+++ ecomDjango/ecomSite/ecomSite/settings.py
@@ -10,12 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 from my_vars.vars import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = [BASE_DIR / 'templates']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = [os.path.join(BASE_DIR, 'templates'),]
+STATIC_DIR = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT_DIR = os.path.join(BASE_DIR, 'assets')
+MEDIA_ROOT_DIR = os.path.join(BASE_DIR, 'media')
 
 
 
@@ -94,7 +97,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -136,12 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [ 'ecomSite/static', ]
+STATICFILES_DIRS = STATIC_DIR
+STATIC_ROOT = STATIC_ROOT_DIR
 
 # media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = MEDIA_ROOT_DIR
 
 
 from django.contrib.messages import constants as messages
