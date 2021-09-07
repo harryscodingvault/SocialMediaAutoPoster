52971fd9b341dd781bf1b51fe0b399d0ca2e1e57
Harry <95robots@gmail.com>
2021-09-07 00:01:26 -0700

    Delete Django/DjangoEcom/ecomSite directory

--- Django/DjangoEcom/ecomSite/orders/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Order
-
-class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ['name', 'phone', 'email', 'address', 'zipcode', 'state', 'city', 'order_note']
